cfaf4760acf13d576f9425476554f73d61bbd446
Sumiza <64884509+Sumiza@users.noreply.github.com>
2021-12-18 17:22:37 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ if [ -n "$discordtokenlink" ]; then
 echo $'#--login info start
 whotemp=$(who | tail -n1 | tr  -s \' \' | tr -d \'()\')
 
-locinfo=$(curl -s ipinfo.io/"$(cut -d \' \' -f5 <<< "$whotemp")")
+locinfo=$(curl -s ipinfo.io/"$(rev <<< "$whotemp" | cut -d \' \' -f1 | rev)")
 
 body="Login detected at $(date) for:
 Hostname        : $(hostname)
@@ -18,7 +18,7 @@ IP(s)           : $(hostname -I)
 User information:
 User            : $(cut -d \' \' -f1 <<< "$whotemp")
 User IP         : $(cut -d \' \' -f5 <<< "$whotemp")
-User Hostname   : $(host "$(cut -d \' \' -f5 <<< "$whotemp")" | rev | cut -d \' \' -f1 | rev)
+User Hostname   : $(grep <<< "$locinfo" \'"hostname"\' | cut -d \'"\' -f4)
 User Location   : $(grep <<< "$locinfo" \'"city"\' | cut -d \'"\' -f4), $(grep <<< "$locinfo" \'"region"\' | cut -d \'"\' -f4), $(grep <<< "$locinfo" \'"country"\' | cut -d \'"\' -f4)
 User ISP        : $(grep <<< "$locinfo" \'"org"\' | cut -d \'"\' -f4)"
 echo "$body"
